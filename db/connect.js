fbd8db124d86706b220aff01198e61c872507964
choehyeonjin <guswls2616@naver.com>
2025-08-07 03:31:45 +0900

    Feat: api 구현

--- db/connect.js
+++ db/connect.js
@@ -1,18 +1,15 @@
-// 테스트용 코드
 const sqlite3 = require('sqlite3').verbose();
-const db = new sqlite3.Database('./app.db');
+const db = new sqlite3.Database('./db/app.db');
 module.exports = db;
 
-// 33개 부서 목록 전체 출력
-db.all("SELECT * FROM departments", (err, rows) => {
-  if (err) return console.error(err.message);
-  console.log(rows); 
-});
+// // 33개 부서 목록 전체 출력
+// db.all("SELECT * FROM departments", (err, rows) => {
+//   if (err) return console.error(err.message);
+//   console.log(rows); 
+// });
 
-// 54개 notice 전체 출력
-db.all("SELECT * FROM notice", (err, rows) => {
-  if (err) return console.error(err.message);
-  console.log(rows);
-});
-
-db.close()
+// // 54개 notice 전체 출력
+// db.all("SELECT * FROM notice", (err, rows) => {
+//   if (err) return console.error(err.message);
+//   console.log(rows);
+// });
